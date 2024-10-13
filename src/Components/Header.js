2de786778cbaa08c00696fe38b5f9b76a8f6c7a3
Shahbaz Siddique <shab291@gmail.com>
2024-10-13 22:45:07 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from '../Assets/logo.png';
 import sign from '../Assets/sign.png';
 import {Link, NavLink, useLocation, useNavigate} from 'react-router-dom';
-import { IoSearch } from 'react-icons/io5';
+import { IoSearchSharp } from 'react-icons/io5';
 import {navigation} from '../Contents/navigation'
 
 
@@ -63,7 +63,7 @@ const Header = () => {
                 />
 
                 <button className='text-2xl text-white'>
-                <IoSearch/>
+                <IoSearchSharp/>
                 </button>
               </form>
 
