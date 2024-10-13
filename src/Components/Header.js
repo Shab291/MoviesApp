eb36607ac7414921a264c5a360771f5b3a5bf58c
Shahbaz Siddique <shab291@gmail.com>
2024-10-13 22:24:50 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import logo from '../Assets/logo.png';
 import sign from '../Assets/sign.png';
 import {Link, NavLink, useLocation, useNavigate} from 'react-router-dom';
-import { IoSearch } from "react-icons/io5";
-import {navigation} from "../Contents/navigation"
+import { IoSearch } from 'react-icons/io5';
+import {navigation} from '../Contents/navigation'
 
 
 
